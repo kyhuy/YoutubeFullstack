d92e8b850ff32394c4b19c0763c0492d32dfa691
huy <kyhuytram@gmail.com>
2021-07-17 16:18:35 +0700

    review MVC

--- NodeJS/src/route/web.js
+++ NodeJS/src/route/web.js
@@ -1,11 +1,12 @@
 import express from "express";
-
+import homeController from "../controller/homeController";
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-  router.get("/", (req, res) => {
-    return res.send("Hello world");
-  });
+  router.get("/", homeController.getHomePage);
+  router.get("/hoidanit", homeController.getAboutPage);
+  //rest api
+
   return app.use("/", router);
 };
 module.exports = initWebRoutes;
