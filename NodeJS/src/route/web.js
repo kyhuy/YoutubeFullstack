782f3f6d39a782e144712fb9130080b052b994b6
kyhuy <kyhuytram@gmail.com>
2021-08-26 15:31:13 +0700

    video 63 get top doctor

--- NodeJS/src/route/web.js
+++ NodeJS/src/route/web.js
@@ -1,6 +1,7 @@
 import express from "express";
 import homeController from "../controller/homeController";
 import userController from "../controller/userController";
+import doctorController from "../controller/doctorController";
 let router = express.Router();
 
 let initWebRoutes = (app) => {
@@ -20,6 +21,7 @@ let initWebRoutes = (app) => {
   router.delete("/api/delete-user", userController.handleDeleteUser);
   //rest api
   router.get("/api/allcode", userController.getAllCode);
+  router.get("/api/top-doctor-home", doctorController.getTopDoctorHome);
   return app.use("/", router);
 };
 module.exports = initWebRoutes;
