bb9e24c3ed9765076d3683380ccbde6fc6c1537b
kyhuy <kyhuytram@gmail.com>
2021-10-04 21:25:28 +0700

    video 68

--- NodeJS/src/route/web.js
+++ NodeJS/src/route/web.js
@@ -22,6 +22,12 @@ let initWebRoutes = (app) => {
   //rest api
   router.get("/api/allcode", userController.getAllCode);
   router.get("/api/top-doctor-home", doctorController.getTopDoctorHome);
+  router.get("/api/get-all-doctors", doctorController.getAllDoctors);
+  router.post("/api/save-infor-doctors", doctorController.postInforDoctor);
+  router.get(
+    "/api/get-detail-doctor-by-id",
+    doctorController.getDetailDoctorById
+  );
   return app.use("/", router);
 };
 module.exports = initWebRoutes;
